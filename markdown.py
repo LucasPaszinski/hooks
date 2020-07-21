ffc713302c128e800cf4296e99f908a73ea0d48c
Lucas L Paszinski <lucas.paszinski@altus.com.br>
2020-07-21 10:08:37 -0300

    Markdown helper improved.

--- markdown.py
+++ markdown.py
@@ -3,7 +3,12 @@ import os
 
 
 def create_markdown_empty_file(root, file):
-    [open(md_path, "a") for md_path in markdown_on_folder_on_root(root, file)]
+    for md_path in markdown_on_folder_on_root(root, file):
+        open(md_path, "a")
+
+def remove_markdown_file(root, file):
+    for md_path in markdown_on_folder_on_root(root, file):
+        os.remove(md_path)
 
 
 def join_markdown_top_title(root, file):
@@ -29,12 +34,15 @@ def join_markdown_top_title(root, file):
 
 
 def folder_on_root(root):
-    return [os.path.join(root, folder)
-            for folder in os.listdir(root) if os.path.isdir(folder)]
+    for folder in os.listdir(root):
+        dir_path = os.path.join(root, folder)         
+        if os.path.isdir(dir_path):
+            yield dir_path
 
 
 def markdown_on_folder_on_root(root, file):
-    return [os.path.join(folder, file) for folder in folder_on_root(root)]
+    for folder in folder_on_root(root):
+        yield os.path.join(folder, file) 
 
 
 def add_new_version_on_markdown(root, file, new_version_text):
@@ -45,8 +53,20 @@ def add_new_version_on_markdown(root, file, new_version_text):
             f.write(f"# {new_version_text}\n")
             f.writelines(lines)
 
-
-create_markdown_empty_file(".", "email.md")
-add_new_version_on_markdown(".", "email.md", "MasterTool 3.30 Beta 04")
-create_markdown_empty_file(".", "release_notes.md")
-join_markdown_top_title(".", "email.md")
+if __name__ == "__main__":
+    root = "..\\.."
+    # # Create new Markdown files
+    # create_markdown_empty_file(root, "email.md")
+    # create_markdown_empty_file(root, "release_notes.md")
+    
+    # # Remove Markdown files
+    # remove_markdown_file(root, "email.md")
+    # remove_markdown_file(root, "release_notes.md")
+    
+    # # Add new version title tag on markdown
+    # add_new_version_on_markdown(root, "email.md", "MasterTool 3.30 Beta 05")
+    # add_new_version_on_markdown(root, "release_notes.md", "MasterTool 3.30 Beta 05")
+    
+    # # Write joined_file.md with latest changes
+    # join_markdown_top_title(".", "email.md")
+    # join_markdown_top_title(".", "release_notes.md")
